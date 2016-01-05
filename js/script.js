e7faaf6b8bb0fddfe1964879e330a47587110ae4
Jose Guardiola <jguardiola9510@gmail.com>
2016-01-04 19:27:54 -0600

    new main

--- js/script.js
+++ js/script.js
@@ -12,11 +12,14 @@ var App = angular.module('App', ['ngRoute'] );
                 templateUrl : '../signup.html',
                 controller  : 'signupCtrl'
             })
+            .when('/teambuilding', {
+                templateUrl : 'pages/teambuilding.html',
+                controller  : 'tbCtrl'
+            })
 
 
 
      });
-
 
    App.controller('mainCtrl', function($scope,$http) {
         // create a message to display in our view
@@ -26,3 +29,7 @@ var App = angular.module('App', ['ngRoute'] );
          // create a message to display in our view
          $scope.message = 'HOLA';
      });
+     App.controller('tbCtrl', function($scope,$http) {
+          // create a message to display in our view
+          $scope.message = 'HOLA';
+      });
